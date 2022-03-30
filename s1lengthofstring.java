70cc6d71b5fc622c67795b8d767d7c67495211e6
Ankitha-reddie <100707145+Ankitha-reddie@users.noreply.github.com>
2022-03-31 01:39:59 +0530

    Create s1lengthofstring.java

---
+++ s1lengthofstring.java
@@ -0,0 +1,13 @@
+//length of string
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        
+       
+        System.out.println("enter the string");
+        Scanner sc=new Scanner(System.in);
+        String =sc.nextLine();
+        
+            System.out.println(str.length());
+    }
+}
