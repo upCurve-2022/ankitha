33575a1908aeeeecc9ddd3946913a3b6a8d00f48
Ankitha-reddie <100707145+Ankitha-reddie@users.noreply.github.com>
2022-03-31 01:43:12 +0530

    Create s1stingimmutable.java

---
+++ s1stingimmutable.java
@@ -0,0 +1,12 @@
+// prove string is immutable
+import java.util.*;
+class Main{
+    public static void main(String[] args) {
+        
+        System.out.println("enter the string");
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+          String str1=sc.nextLine();
+       System.out.println(str+str1);
+}
+}
