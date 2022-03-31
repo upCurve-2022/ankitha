bf0aefb6b9ebdcada23bafa4f94b9dd8b39d7cfa
Ankitha-reddie <100707145+Ankitha-reddie@users.noreply.github.com>
2022-03-31 23:29:40 +0530

    Create javachallengequestion12.java

---
+++ javachallengequestion12.java
@@ -0,0 +1,13 @@
+import java.lang.*;
+public class StringDemo {
+   public static void main(String[] args) {
+      String str1 = "target";
+      System.out.println(str1);
+      String str2 = str1.concat(" up");
+      System.out.println(str2);
+     
+
+      String str3 = str2.concat("curve");
+      System.out.println(str3);
+   }
+}
