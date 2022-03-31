5329fefdc5653304192e5be7989b86c5e85a2558
Ankitha-reddie <100707145+Ankitha-reddie@users.noreply.github.com>
2022-03-31 23:13:25 +0530

    Create javachallengequestion4.java

---
+++ javachallengequestion4.java
@@ -0,0 +1,13 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        char ch = 'b';
+        int ascii = ch;
+        // You can also cast char to int
+        int castAscii = (int) ch;
+
+        System.out.println("The ASCII value of " + ch + " is: " + ascii);
+        System.out.println("The ASCII value of " + ch + " is: " + castAscii);
+    }
+}
