d35103797ab8ed9c2ceabd1e91fdf738d87ef0e3
Ankitha-reddie <100707145+Ankitha-reddie@users.noreply.github.com>
2022-03-31 23:58:52 +0530

    Create javachallengequestion20.java

---
+++ javachallengequestion20.java
@@ -0,0 +1,8 @@
+public class Main {
+   public static void main(String[] args) {
+      String str1 = "I am always ready to learn although I do not always like being taught.";
+      System.out.println("String: "+str1);
+      String str2 = str1.replace('a', '$');  
+      System.out.println("Updated string: "+str2);
+   }
+}
